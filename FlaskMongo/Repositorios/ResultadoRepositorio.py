d671ffe089a4cf30a013d09716c163dd9d5e18b4
LauraECS <laura.cancino92@gmail.com>
2022-10-31 20:44:22 -0500

    Carpeta de repositorios finalizada.

---
+++ FlaskMongo/Repositorios/ResultadoRepositorio.py
@@ -0,0 +1,30 @@
+from Repositorios.InterfazRepositorio import InterfazRepositorio
+from Modelos.Resultado import Resultado
+from bson import ObjectId
+
+class ResultadoRepositorio(InterfazRepositorio[Resultado]):
+    #votaciones por mesa
+    def getListadoCandidatosInscritosMesa(self, id_mesa):
+        theQuery = {"mesa.$id": ObjectId(id_mesa)}
+        return self.query(theQuery)
+
+    #votaciones por candidato
+    def getListadoMesasCandidatoInscrito(self, id_candidato):
+        theQuery = {"candidato.$id": ObjectId(id_candidato)}
+        return self.query(theQuery)
+
+    #número mayor de una cédula
+    def getNumeroCedulaMayorCandidato(self):
+        query = {
+            "$group":{
+                "_id": "$candidato",
+                "max": {
+                    "$max": "$cedula"
+                },
+                "doc": {
+                    "$first": "$$ROOT"
+                }
+            }
+        }
+        pipeline = [query]
+        return self.queryAggregation(pipeline)
