0792e40ce032dc697f0d1699302784530131ff6d
LauraECS <laura.cancino92@gmail.com>
2022-11-14 17:12:33 -0500

    backend de resultados finalizado

--- FlaskMongo/Repositorios/InterfazRepositorio.py
+++ FlaskMongo/Repositorios/InterfazRepositorio.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from bson import DBRef
 from bson.objectid import ObjectId
 from typing import TypeVar, Generic, List,  get_origin, get_args
@@ -21,8 +22,7 @@ class InterfazRepositorio(Generic[T]):
             value["_id"] = value["_id"].__str__()
             newList.append(value)
         return newList
-    
-    #obtiene los valores de dbref
+    #Obtiene los valores de dbref
     def getValuesDBRef(self, x):
         keys = x.keys()
         for k in keys:
@@ -37,8 +37,7 @@ class InterfazRepositorio(Generic[T]):
             elif isinstance(x[k], dict):
                 x[k]= self.getValuesDBRef(x[k])
         return x
-    
-    #función buscar por Id
+    #Función buscar por Id
     def findById(self, id):
         laColeccion = self.db[self.collection]
         x = laColeccion.find_one({"_id": ObjectId(id)})
@@ -58,7 +57,6 @@ class InterfazRepositorio(Generic[T]):
         if len(newList) == 0:
             newList = x
         return newList
-    
     #transformamos los objetos en sus IDs
     def transformObjectIds(self, x):
         for attribute in x.keys():
@@ -69,8 +67,7 @@ class InterfazRepositorio(Generic[T]):
             elif isinstance(x[attribute], dict):
                 x[attribute]= self.transformObjectIds(x[attribute])
         return x
-    
-    #buscar todo en una colección
+    #Buscar todo en una collection
     def findAll(self):
         laColeccion = self.db[self.collection]
         data = []
@@ -80,8 +77,7 @@ class InterfazRepositorio(Generic[T]):
             x = self.getValuesDBRef(x)
             data.append(x)
         return data
-    
-    #update
+    #Update
     def update(self, id, item: T):
         _id = ObjectId(id)
         laColeccion = self.db[self.collection]
@@ -90,18 +86,15 @@ class InterfazRepositorio(Generic[T]):
         updateItem = {"$set": item}
         x = laColeccion.update_one({"_id":_id}, updateItem)
         return {"updated_count":x.matched_count}
-    
     #delete
     def delete(self, id):
         laColeccion = self.db[self.collection]
         cuenta = laColeccion.delete_one({"_id": ObjectId(id)}).deleted_count
         return {"deleted_count": cuenta}
-    
     #obtener objetos desde una dbref
     def ObjectToDBRef(self, item: T):
         nameCollection = item.__class__.__name__.lower()
         return DBRef(nameCollection, ObjectId(item._id))
-    
     #transforma las DBRefs
     def transformRefs(self, item):
         theDict = item.__dict__
@@ -111,7 +104,6 @@ class InterfazRepositorio(Generic[T]):
                 newObject = self.ObjectToDBRef(getattr(item,k))
                 setattr(item, k, newObject)
         return item
-    
     #guardar
     def save(self, item: T):
         laColeccion = self.db[self.collection]
@@ -131,8 +123,7 @@ class InterfazRepositorio(Generic[T]):
         x = laColeccion.find_one({"_id":ObjectId(elId)})
         x["_id"] = x["_id"].__str__()
         return self.findById(elId)
-    
-    #querys específicos
+    #Querys especificos
     def query(self, theQuery):
         laColeccion = self.db[self.collection]
         data = []
@@ -142,8 +133,7 @@ class InterfazRepositorio(Generic[T]):
             x = self.getValuesDBRef(x)
             data.append(x)
         return data
-    
-    #query agregación
+    #Query agregación
     def queryAggregation(self, theQuery):
         laColeccion = self.db[self.collection]
         data = []
@@ -153,3 +143,4 @@ class InterfazRepositorio(Generic[T]):
             x = self.getValuesDBRef(x)
             data.append(x)
         return data
+        
